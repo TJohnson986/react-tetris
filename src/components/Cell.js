66c12204d2ce443667cd4fe753b43301f24bdf11
Tyler Johnson <tyler@csdconsultants.us>
2021-12-14 16:44:51 -0700

    creates styled components directory, creates styling using styled components for Cell component

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { StyledCell } from './styles/StyledCell';
+import { TETROMINOS } from '../tetrominos';
+
 
 const Cell = ({ type }) => (
-    <div>This is Cell</div>
+    <StyledCell type={type} color={TETROMINOS[type].color}>This is Cell</StyledCell>
 );
 
 export default Cell;
