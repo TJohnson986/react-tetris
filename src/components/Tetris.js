836fcdff3a9ed565a7b965d77c5b986f544d52f1
Tyler Johnson <tyler@csdconsultants.us>
2021-12-14 21:23:13 -0700

    creates initial state in Tetris component for gameOver, dropTime, player, and stage

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -11,18 +11,26 @@ import { usePlayer } from '../hooks/usePlayer';
 import { useStage } from '../hooks/useStage';
 
 const Tetris = () => {
-    
+    const [dropTime, setDropTime] = useState(null);
+    const [gameOver, setGameOver] = useState(false);
+
+    const [player] = usePlayer();
+    const [ stage, setStage ] = useStage(player);
 
     return (
         <StyledTetrisWrapper>
             <StyledTetris>
-            <Stage stage={createStage()} />
+            <Stage stage={stage} />
             <aside>
+                {gameOver ? (
+                    <Display gameOver={gameOver} />
+                ) : (
                 <div>
                 <Display text="Score" />
                 <Display text="Rows" />
                 <Display text="Level" />
                 </div>
+                )};
                 <StartButton />
             </aside>
             </StyledTetris>
